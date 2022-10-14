74afc48efd4117ef593ac696ad5360f54f1132fa
Noah Lundquist <nalundquist@gmail.com>
2022-10-14 15:49:17 -0700

    WIP: bugfixing incidents/experimenting

---
+++ Factory/Migrations/20221014220519_Secondary.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Secondary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "BeingRepaired",
+                table: "Machines",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Functional",
+                table: "Machines",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Incidents",
+                columns: table => new
+                {
+                    IncidentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MachineId = table.Column<int>(type: "int", nullable: false),
+                    EngineerId = table.Column<int>(type: "int", nullable: false),
+                    MalfunctionDate = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    RepairDate = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Open = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidents", x => x.IncidentId);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Engineers_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Engineers",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_EngineerId",
+                table: "Incidents",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_MachineId",
+                table: "Incidents",
+                column: "MachineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Incidents");
+
+            migrationBuilder.DropColumn(
+                name: "BeingRepaired",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Functional",
+                table: "Machines");
+        }
+    }
+}
