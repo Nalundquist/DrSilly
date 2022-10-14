8171e9eab965a0b1c798037d1900652b8f239ce0
Noah Lundquist <nalundquist@gmail.com>
2022-10-14 09:59:24 -0700

    add all Controllers, Home Views

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -4,3 +4,90 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using Factory.Models;
+
+namespace Factory.Controllers
+{
+	public class MachinesController : Controllers
+	{
+		private readonly FactoryContext _db;
+		public MachinesController(FactoryContext db)
+		{
+			_db = db;
+		}
+
+		public ActionResult Index()
+		{
+			List<Machine> machines = _db.Machines.ToList();
+			return View(machines);
+		}
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Machine machine)
+		{
+			_db.Machines.Add(machine);
+			_db.SaveChanges();
+			return RedirectToAction("Details", new {id = machine.MachineId});
+		}
+
+		public ActionResult Details(int id)
+		{
+			Machine thisMachine = _db.Machines
+				.Include(machine => machine.JoinEngMach)
+				.ThenInclude(join => join.Machine)
+				.FirstOrDefault(machine => machine.MachineId == id);
+			return View(thisMachine);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+			return View(thisMachine);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Machine machine)
+		{
+			_db.Entry(machine).State = EntityState.Modified;
+			_db.SaveChanges();
+			return RedirectToAction("Details", new {id = machine.MachineId});
+		}
+
+		[HttpPost]
+		public ActionResult Delete(int id)
+		{
+			Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+			_db.Machines.Remove(thisMachine);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult AddEngineer(int id)
+		{
+			Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+			ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+			return View(thisMachine);
+		}
+
+		[HttpPost]
+		public ActionResult AddEngineer(Machine machine, int EngineerId)
+		{
+			_db.EngineerMachine.Add(new EngineerMachine() {MachineId = machine.MachineId, EngineerId = EngineerId});
+			_db.SaveChanges();
+			return RedirectToAction("Details", new {id = machine.MachineId});
+		}
+
+		[HttpPost]
+		public ActionResult DeleteEngineer(int id)
+		{
+			EngineerMachine thisJoin = _db.EngineerMachine.FirstOrDefault(join => join.EngineerMachineId == id);
+			_db.MachineMachine.Remove(thisJoin);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
